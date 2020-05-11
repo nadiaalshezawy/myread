bce3a088b927f9e37853fe59d5365fa160ac2594
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-05-12 02:34:43 +0400

    adding the routing oprtion

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import SearchBar from "./SearchBar";
 import "./App.css";
 
 class BooksApp extends React.Component {
   render() {
     return (
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-        <div className="open-search">
-          <button>Add a book</button>
-        </div>
+      <div>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+              <div className="open-search">
+                <button>Add a book</button>
+              </div>
+            </div>
+          )}
+        />
+        <Route path="/search" render={() => <SearchBar />} />
       </div>
     );
   }
