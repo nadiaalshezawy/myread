66bf11a13c8ab4cedd1654e80629bc5aa6d6e9ef
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-05-17 11:42:04 +0400

    fixed the path file in css

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,63 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import "./App.css";
+import * as BooksAPI from "../apis/BooksAPI";
 
 class BooksApp extends React.Component {
+  /*
+
+  //how to get the result another way
+    ///.then((response) => {
+    ///console.log(response.data.results);
+    /// });
+
+  fetch('https://api.mydomain.com')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+   onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+
+fetch(`${api}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  }).then(res => res.json())
+    .then(data => data.books)
+  
+  
+  export const search = (query) =>
+  fetch(`${api}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  }).then(res => res.json())
+    .then(data => data.books)
+*/
+
+  onSearchSubmit(term) {
+    console.log(term);
+    BooksAPI.search(term)
+      .then((response) => {
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  }
   render() {
+    //the main app have two route main page or search
     return (
-      <div>
+      <div className="app">
         <Route
           exact
           path="/"
@@ -14,12 +65,17 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
               <div className="open-search">
-                <button>Add a book</button>
+                <Link to="/search">
+                  <button type="button">add book</button>
+                </Link>
               </div>
             </div>
           )}
         />
-        <Route path="/search" render={() => <SearchBar />} />
+        <Route
+          path="/search"
+          render={() => <SearchBar onTermSubmit={this.onSearchSubmit} />}
+        />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 class SearchBar extends React.Component {
+  state = { term: " " };
+
+  //when input change update term value
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+    this.props.onTermSubmit(this.state.term);
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onTermSubmit(this.state.term);
+  };
+
   render() {
-    return <div>search</div>;
+    return (
+      <div className="app">
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link to="/">
+              <button className="close-search">Close</button>
+            </Link>
+            <form onSubmit={this.onFormSubmit}>
+              <div className="search-books-input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Search by title or author "
+                  onChange={this.onInputChange.bind(this)}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
